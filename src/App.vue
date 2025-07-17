d5152c4db288cb5ebdbb6ba0541569986bc6b6b9
tumo-edgar <edgar.nikoghosyan1@tumo.org>
2025-07-17 19:32:45 +0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <Card :abc="number" />
+    <button @click="increment">Click me</button>
+</template>
+
+<script setup>
+  import {ref} from 'vue';
+  import Card from './components/Card.vue'
+
+  let number = ref(0)
+
+  const increment = () => {
+    number.value++
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+    font-weight: 900;
+    font-size: 50px;
+    margin: 100px;
+  }
+  button {
+    margin-left: 100px;
+  }
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>{{props.abc}}</h1>
+      <p>asdhjbasjhbasjhbsad</p>
+      <input type="text" name="" id="">
+      <h6>sdfhbasdjfhbsd</h6>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    let props = defineProps(['abc'])
+</script>
+
+<style>
+
+</style>
